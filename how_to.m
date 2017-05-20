f73f810028f58408b8c4bec064c4e37c9c82640c
Neil Alishev <alishev.neil@gmail.com>
2017-05-20 18:15:32 +0300

    Add HOWTO function

---
+++ how_to.m
@@ -0,0 +1,29 @@
+% HOWTO function
+function [] = how_to(t_matrix)
+
+% limit of iterations.
+L = 3000;
+
+% step size
+ALPHA = 0.2;
+
+robot = get_robot;
+workspace_axis = [-2 2 -2 2 -0.5 10];
+
+% normalize transformation matrix to make it orthogonal.
+[U, ~, V]=svd(t_matrix(1:3, 1:3));
+
+t_corrected = U*V';
+t_matrix = [t_corrected, t_matrix(1:3, end); 0 0 0 1];
+
+q = robot.ikine(t_matrix, 'ilimit', L, 'alpha', ALPHA);
+
+robot.plot(q, 'workspace', workspace_axis);
+robot.teach();
+
+% convert result to dergees.
+q = arrayfun(@(z) radtodeg(z), q);
+disp('IK: joint angles');
+disp(q);
+    
+end
