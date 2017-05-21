c84c97715c90ffc459c0290dd13ffbfc8bf509f3
Neil Alishev <alishev.neil@gmail.com>
2017-05-21 19:36:27 +0300

    show joint pairs workspace

--- ws_display.m
+++ ws_display.m
@@ -54,10 +54,74 @@ for theta1 = 0 : RATE : 2*pi
     c(end + 1) = 1.0;
 end
 
+f = figure();
+set(f,'name','Separate joint workspaces','numbertitle','off');
 scatter3(x, y, z, 30, c);
 plot_robot(robot, [-7 7 -7 7 -0.5 10]);
 
-RATE = pi / 3;
+RATE = pi / 18;
+x = [];
+y = [];
+z = [];
+
+% plot for 4-5 joint pair
+for theta4 = 0 : RATE : 2*pi
+    for theta5 = 0 : RATE : 2*pi
+        t = robot.fkine([0 -pi/2 0 theta4 theta5 0]);
+        pos = t(:,4);
+        x(end + 1) = pos(1);
+        y(end + 1) = pos(2);
+        z(end + 1) = pos(3);
+    end
+end
+
+f = figure();
+set(f,'name','4-5 joint pair workspace','numbertitle','off');
+scatter3(x, y, z);
+plot_robot(robot, [-7 7 -7 7 -0.5 10]);
+
+x = [];
+y = [];
+z = [];
+
+% plot for 3-4 joint pair
+for theta2 = -pi : RATE : 0
+    for theta3 = pi/60 : RATE : 2*pi - pi/60
+        t = robot.fkine([0 theta2 theta3 pi/2 0 0]);
+        pos = t(:,4);
+        x(end + 1) = pos(1);
+        y(end + 1) = pos(2);
+        z(end + 1) = pos(3);
+    end
+end
+
+f = figure();
+set(f,'name','2-3 joint pair workspace','numbertitle','off');
+scatter3(x, y, z);
+plot_robot(robot, [-7 7 -7 7 -0.5 10]);
+
+x = [];
+y = [];
+z = [];
+
+% plot for 1-2 joint pair
+for theta1 = 0 : RATE : 2*pi
+    for theta2 = -pi : RATE : 0
+        t = robot.fkine([theta1 theta2 0 pi/2 0 0]);
+        pos = t(:,4);
+        x(end + 1) = pos(1);
+        y(end + 1) = pos(2);
+        z(end + 1) = pos(3);
+    end
+end
+
+f = figure();
+set(f,'name','1-2 joint pair workspace','numbertitle','off');
+scatter3(x, y, z);
+plot_robot(robot, [-7 7 -7 7 -0.5 10]);
+
+
+RATE = pi/4;
 x = [];
 y = [];
 z = [];
@@ -79,8 +143,9 @@ for theta1 = 0 : RATE : 2*pi
     end
 end
 
-figure();
-scatter3(x, y, z);
-plot_robot(robot, [-5 5 -5 5 -0.5 10]);
+f = figure();
+set(f,'name','Overall robot workspace','numbertitle','off');
+scatter3(x, y, z, 1);
+plot_robot(robot, [-7 7 -7 7 -0.5 10]);
 
 end
