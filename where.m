27ace749acbf4899a36fc8e018fc57ac32320a10
Neil Alishev <alishev.neil@gmail.com>
2017-05-22 12:53:19 +0300

    enhance validation

--- where.m
+++ where.m
@@ -3,8 +3,8 @@ function [t_matrix] = where(q)
 
 % validate joint angles
 theta2 = q(2);
-if theta2 > 0 || theta2 < -pi/2
-    throw(MException('where:theta2', 'theta2 must be in [-pi/2; 0]'));
+if theta2 > 0 || theta2 < -pi
+    throw(MException('where:theta2', 'theta2 must be in [-pi; 0]'));
 end
 
 robot = get_robot;
